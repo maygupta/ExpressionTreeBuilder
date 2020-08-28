742e3c559afcce854299d5e28205d67a0fa62ddf
Mayank Gupta <mgupta8@apple.com>
2020-08-27 23:05:12 -0700

    indent using spaces

--- src/main/java/visit/Visitor.java
+++ src/main/java/visit/Visitor.java
@@ -5,7 +5,7 @@ import node.Node;
 import node.OperatorNode;
 
 public interface Visitor {
-	void visit(Node node);
-	void visit(LeafNode node);
-	void visit(OperatorNode node);
+  void visit(Node node);
+  void visit(LeafNode node);
+  void visit(OperatorNode node);
 }
